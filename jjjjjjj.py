b3d3d401e31e10df3bf0e64961512ad6a116593a
Angelinariabceva <angelinariabceva@gmail.com>
2025-09-03 10:38:52 +0300

    mmm

---
+++ jjjjjjj.py
@@ -0,0 +1,38 @@
+print(1+1)
+print ("1"+"1")
+#print("1"+1) # keelatud
+# Andme tüübid
+# "hello world" - str
+# 1 - int
+# 1.5 - float
+
+#muutujad
+name = "Avvvv"
+print(name)
+perekonnanimi = "EWyhxb"
+print (name+ " " + perekonnanimi)
+
+# luua muutujad film, aasta, rezisoor, naitleja1 ja naitleja2
+# tulemus: film matrix ilmus aastal 1999, rezisöör on wachowski
+# ning peanäirlejad on Keanu Reeves ja Laurence Fishburne
+
+film= "m"
+year= "1999"
+rezisoor= "Kdsssa"
+naitleja1= "hhhh"
+natleja2= "dddd"
+
+
+print("film" + film + " ilmus asastal " + year + " rezisoor on " + rezisoor + " ning peanailtjead on " )
+
+lemmikloom = input("Sisesta oma lemikloom: ")
+print(Minu lemmikloom on: "kass" + lemmikloom)
+
+pikkus = input("sisesta pikkus: ")
+laius = input("sisesta laius: ")
+umbermot = 2 * (pikkus + laius)
+pindala = pikkus * laius
+print(ümbermõõt: "1", umbermot)
+print("laius: ", laius)
+
+
